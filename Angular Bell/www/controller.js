d0709b14cf9ecaa576b7b83fc9846cebb8db559c
Craig <craigdanielbell@gmail.com>
2016-10-31 12:21:28 +0000

    First Commit
    Built a single page to-do list, mobile web application using Angular.js,
    and packaged it with PhoneGap Build.

---
+++ Angular Bell/www/controller.js
@@ -0,0 +1,23 @@
+
+angular.module('ToDo',[]).
+
+controller('todoController',['$scope',function($scope){
+	$scope.todos = [
+		{'title':'Build a todo app','done':false}
+	];
+	
+	$scope.getTotalTodos = function () {
+		return $scope.todos.length;
+	};
+	
+	$scope.addTodo = function(){
+		$scope.todos.push({'title':$scope.newTodo,'done':false})
+		$scope.newTodo = ''
+	}
+	
+	$scope.clearCompleted = function(){
+		$scope.todos = $scope.todos.filter(function(item){
+			return !item.done
+		})
+	}
+}])
